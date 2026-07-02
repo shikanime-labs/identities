6d3fb4396d29bb13344907dde5ba960c02a8168c
Shikanime Deva <william.phetsinorath@shikanime.studio>
2026-07-02 19:20:42 +0200

    Add individual SOPS identities option
    Signed-off-by: Shikanime Deva <william.phetsinorath@shikanime.studio>
    Change-Id: I9dde19f5bdbad8a9fbe6a3d4edc907596a6a6964

--- modules/devenv/identities.nix
+++ modules/devenv/identities.nix
@@ -13,6 +13,10 @@ with lib;
     sops.enable = mkEnableOption "Enable SOPS encryption" // {
       default = config.sops.enable && config.identities.autoEnable;
     };
+
+    nixtar.enable = mkEnableOption "Enable nixtar";
+
+    telsha.enable = mkEnableOption "Enable telsha";
   };
 
   config = mkIf config.identities.enable {
@@ -21,13 +25,11 @@ with lib;
         {
           path_regex = ".*";
           age =
-            let
-              telsha = "age1pwl9yz4k4255a4h8qz7lafce8wxhsul0cnqwmr8528fqgujlfshshv3z3g";
-              nixtar = "age1um232l0h8wn9mtha2qf4f4mnf7ucjayvf5qxjvynatmasg8qg5mshekvjl";
-            in
-            [
-              telsha
-              nixtar
+            optionals config.identities.nixtar.enable [
+              "age1um232l0h8wn9mtha2qf4f4mnf7ucjayvf5qxjvynatmasg8qg5mshekvjl"
+            ]
+            ++ optionals config.identities.telsha.enable [
+              "age1pwl9yz4k4255a4h8qz7lafce8wxhsul0cnqwmr8528fqgujlfshshv3z3g"
             ];
         }
       ];
